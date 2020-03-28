e8719e65e6377bd8fb79b46932c5500a17b586aa
Pedro Lima <55360675+pedro-limma@users.noreply.github.com>
2020-03-28 13:37:57 -0300

    Update index.js

--- mobile/src/pages/Detail/index.js
+++ mobile/src/pages/Detail/index.js
@@ -14,7 +14,7 @@ export default function Detail(){
 
   const incident = route.params.incident; 
 
-  const message = `Olá ${incident.name}, estou entrando em contato pois gostaria de ajudar no caso "${incident.title}" com o valor de ${Intl.NumberFormat('pt-BR', { style:'currency', currency:'BRL' }).format(incident.value)}`
+  const message = `Olá ${incident.name}, estou entrando em contato pois gostaria de ajudar no caso "${incident.titulo}" com o valor de ${Intl.NumberFormat('pt-BR', { style:'currency', currency:'BRL' }).format(incident.value)}`
 
 
   function navigateBack() {
